d4efc81c825f3c6abd99969f2a31ec6f60b1c0d2
Kingori-software <its.kingori.dev@gmail.com>
2026-08-29 08:50:02 +0300

    Create image.js

---
+++ image.js
@@ -0,0 +1,15 @@
+const params = new URLSearchParams(window.location.search);
+
+const image = params.get("img");
+
+const fullImage = document.getElementById("fullImage");
+
+if (image) {
+
+    fullImage.src = image;
+
+} else {
+
+    fullImage.alt = "Image unavailable";
+
+}
