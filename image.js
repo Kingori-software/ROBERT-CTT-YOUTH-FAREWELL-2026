30db117b514673c0dee2fd6aa7fce789881a62de
Kingori-software <its.kingori.dev@gmail.com>
2026-08-29 20:09:34 +0300

    Update image.js

--- image.js
+++ image.js
@@ -1,15 +1,18 @@
-const params = new URLSearchParams(window.location.search);
+const params = new URLSearchParams(
+window.location.search
+);
 
 const image = params.get("img");
 
-const fullImage = document.getElementById("fullImage");
+const fullImage =
+document.getElementById("fullImage");
 
 if (image) {
 
-    fullImage.src = image;
+fullImage.src = image;
 
 } else {
 
-    fullImage.alt = "Image unavailable";
+fullImage.alt = "Image unavailable";
 
 }
